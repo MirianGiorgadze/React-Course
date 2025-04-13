a290379d10a9972f2c823ce872ab042de4a129f5
MirianGiorgadze <giorgadzemiriani@gmail.com>
2025-04-13 13:13:32 +0400

    Update Counter.jsx
    setState 2

--- 1-fundamentals/src/components/states/Counter.jsx
+++ 1-fundamentals/src/components/states/Counter.jsx
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
 
 function Counter() {
-  const [count, setCount] = useState(0); // საწყისი მნიშვნელობა: 0
+  const [count, setCount] = useState(0); 
+  const [word, setWord] = useState("First Word")// საწყისი მნიშვნელობა: 0
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Doe");
+  const [age, setAge] = useState(100);
 
-  const increaseCount = () => {
-    setCount(count * 2); // state-ის განახლება
+  const firstNameInput = document.getElementById("firstNameInput");
+  const lastNameInput = document.getElementById("lastNameInput");
+  const ageInput = document.getElementById("ageInput");
+
+  const changeData = () => {
+    setFirstName(firstNameInput.value);
+    setLastName(lastNameInput.value);
+    setAge(ageInput.value);
+  }
+
+  const increaseCount = (e) => {
+    setCount(count + 1); // state-ის განახლება
   };
 
-
-
-  // let count = 0; // საწყისი მნიშვნელობა: 0
-
-  // const increaseCount = () => {
-  //   count = count + 1; 
-  //   console.log(count);
-  // };
-
+  const changeText = (e) => {
+    setWord(e.target.value);
+    console.log(word);
+  }
   return (
     <div>
-      <p>Count: {count}</p>
+      {/* <p>Count: {count}</p>
       <button onClick={increaseCount}>Add +1</button>
       <p id={"result"}></p>
+      <input type='text' onBlur={changeText}></input>
+      <p style={{color: "red"}}>{word}</p> */}
+      <input type="text" id="firstNameInput" placeholder='სახელი' onChange={changeData}></input>
+      <input type="text" id="lastNameInput" placeholder='გვარი' onChange={changeData}></input>
+      <input type="text" id="ageInput" placeholder='ასაკი' onChange={changeData}></input>
+      <p>სახელი: {firstName}; გვარი: {lastName}; ასაკი: {age}</p>
     </div>
   );
 }
